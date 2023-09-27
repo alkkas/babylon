77fe42f9cd28c77181b0973555f2de8417e6bc4b
Черных Егор Кириллович <e.chernykh@dpractices.local>
2023-09-27 15:48:38 +0300

    create multiple houses

--- src/3d/createHouse.ts
+++ src/3d/createHouse.ts
@@ -1,89 +1,65 @@
 import {
-  Color3,
-  Mesh,
-  MeshBuilder,
-  Scene,
-  StandardMaterial,
-  Texture,
-  Vector3,
+    Color3,
+    Mesh,
+    MeshBuilder, Nullable,
+    Scene,
+    StandardMaterial,
+    Texture,
+    Vector3, Vector4,
 } from '@babylonjs/core'
 
-const createHouse = (scene: Scene): void => {
-  const roofMat = new StandardMaterial('roofMat', scene)
-  roofMat.diffuseTexture = new Texture(
-    'https://assets.babylonjs.com/environments/roof.jpg',
-    scene
-  )
-
-  const houseMat = new StandardMaterial('houseMat')
-  houseMat.diffuseTexture = new Texture(
-    'https://www.babylonjs-playground.com/textures/floor.png',
-    scene
-  )
-
-  const house = MeshBuilder.CreateBox('house', { size: 2 }, scene)
-  house.material = houseMat
-  house.position = new Vector3(0, 1, 0)
-  house.rotation.y = Math.PI / 4
-  const roof = MeshBuilder.CreateCylinder(
-    'roof',
-    { diameter: 1.3, height: 1.2, tessellation: 3 },
-    scene
-  )
-  roof.material = roofMat
-  roof.scaling.y = 2
-  roof.scaling.z = 2
-  roof.rotation.z = Math.PI / 2
-  roof.rotation.y = Math.PI / 4
-  roof.position = new Vector3(-0.1, 2.5, -0.5)
-}
 
 class House {
-  houseBase: Mesh
-  roof: Mesh
-  id: string
+    mesh: Nullable<Mesh>
+    id: string
 
-  static houses: House[] = []
+    static houses: House[] = []
+    static scene: Scene
 
-  constructor(size: number = 2) {
-    const houseId = String(Math.random())
-    this.id = houseId
-    this.houseBase = MeshBuilder.CreateBox('house' + this.id, { size })
-    this.roof = MeshBuilder.CreateCylinder('roof' + this.id, {
-      diameter: 1.3,
-      height: 1.2,
-      tessellation: 3,
-    })
-    House.houses.push(this)
+    constructor(scene: Scene, size: number = 2) {
+        this.id = String(Math.random())
+        if (!House.scene) {
+            House.scene = scene
+        }
+        const roof = MeshBuilder.CreateCylinder('roof' + this.id, {
+            diameter: 1.3,
+            height: 1.2,
+            tessellation: 3,
+        })
 
-    const roofMat = new StandardMaterial('roofMat' + this.id)
-    roofMat.diffuseTexture = new Texture(
-      'https://assets.babylonjs.com/environments/roof.jpg'
-    )
-    this.roof.material = roofMat
-    this.roof.scaling.y = 2
-    this.roof.scaling.z = 2
-    this.roof.rotation.z = Math.PI / 2
-    this.roof.rotation.y = Math.PI / 4
-    this.roof.position = new Vector3(0, 2.3, 0)
 
-    const houseBaseMat = new StandardMaterial('houseMat' + this.id)
-    houseBaseMat.diffuseTexture = new Texture(
-      'https://www.babylonjs-playground.com/textures/floor.png'
-    )
-    this.houseBase.material = houseBaseMat
-    this.houseBase.position = new Vector3(0, 1, 0)
-    this.houseBase.rotation.y = Math.PI / 4
-  }
+        const roofMat = new StandardMaterial('roofMat' + this.id)
+        roofMat.diffuseTexture = new Texture(
+            'https://assets.babylonjs.com/environments/roof.jpg'
+        )
+        roof.material = roofMat
+        roof.scaling.y = 2
+        roof.scaling.z = 2
+        roof.rotation.z = Math.PI / 2
+        roof.position = new Vector3(0, 2.3, 0)
 
-  addToScene(scene: Scene) {
-    scene.addMesh(this.houseBase)
-    scene.addMesh(this.roof)
-  }
-  position(x: number, y: number, z: number) {
-    this.houseBase.position = new Vector3(x, y, z)
-    this.roof.position = new Vector3(x, y + 1.3, z)
-  }
+        const houseBaseMat = new StandardMaterial('houseMat' + this.id)
+        houseBaseMat.diffuseTexture = new Texture(
+            "https://assets.babylonjs.com/environments/cubehouse.png"
+        )
+        const faceUV = [new Vector4(0.5, 0.0, 0.75, 1.0), new Vector4(0.0, 0.0, 0.25, 1.0), new Vector4(0.25, 0, 0.5, 1.0), new Vector4(0.75, 0, 1.0, 1.0)]
+        const houseBase = MeshBuilder.CreateBox('house' + this.id, {size, faceUV, wrap: true})
+        houseBase.material = houseBaseMat
+        houseBase.position = new Vector3(0, 1, 0)
+        this.mesh = Mesh.MergeMeshes([houseBase, roof], true, false, undefined, false, true)
+        if (!this.mesh) return
+        this.mesh.scaling = new Vector3(0.3, 0.3, 0.3)
+        House.houses.push(this)
+    }
+
+    addToScene() {
+        House.scene.addMesh(this.mesh as Mesh)
+    }
+
+    position(x: number, y: number, z: number) {
+        if (!this.mesh) return
+        this.mesh.position = new Vector3(x, y, z)
+    }
 }
 
 export default House
--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,79 @@
 import {
-  FreeCamera,
-  Vector3,
-  HemisphericLight,
-  MeshBuilder,
-  StandardMaterial,
-  Color3,
-  ArcRotateCamera,
+    FreeCamera,
+    Vector3,
+    HemisphericLight,
+    MeshBuilder,
+    StandardMaterial,
+    Color3,
+    ArcRotateCamera,
 } from '@babylonjs/core'
 import BABYLON from '@babylonjs/core'
-import { Scene } from '@babylonjs/core/scene'
-import { Mesh } from '@babylonjs/core/Meshes'
+import {Scene} from '@babylonjs/core/scene'
+import {Mesh} from '@babylonjs/core/Meshes'
 import './App.css'
 import SceneComponent from 'babylonjs-hook'
 import House from './3d/createHouse'
+import {useEffect} from "react";
 
 function App() {
-  let box: Mesh | undefined = undefined
 
-  const onSceneReady = (scene: Scene) => {
-    // This creates and positions a free camera (non-mesh)
-    const camera = new ArcRotateCamera(
-      'Camera',
-      0,
-      0,
-      10,
-      new Vector3(0, 0, 0),
-      scene
+    const onSceneReady = (scene: Scene) => {
+        // This creates and positions a free camera (non-mesh)
+        const camera = new ArcRotateCamera(
+            'Camera',
+            0,
+            0,
+            10,
+            new Vector3(0, 0, 0),
+            scene
+        )
+
+        // This targets the camera to scene origin
+        camera.setPosition(new Vector3(0, 0, 20))
+
+        const canvas = scene.getEngine().getRenderingCanvas()
+        // This attaches the camera to the canvas
+        camera.attachControl(canvas, true)
+
+        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+        const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene)
+
+        // Default intensity is 1. Let's dim the light a small amount
+        light.intensity = 0.8
+        // Our built-in 'ground' shape.
+        const groundMaterial = new StandardMaterial('groundMaterial', scene)
+        groundMaterial.diffuseColor = new Color3(0, 1, 0)
+        const ground = MeshBuilder.CreateGround(
+            'ground',
+            {width: 6, height: 6},
+            scene
+        )
+        ground.material = groundMaterial
+
+        init(scene)
+    }
+
+    const init = (scene: Scene) => {
+        const standaloneHouse = new House(scene)
+        const houses = []
+        if (!standaloneHouse.mesh) return
+        for (let i = 1; i < 9; i++) {
+            houses[i] = standaloneHouse.mesh.createInstance('house' + i)
+            houses[i].position.x = Math.sin(i * 180 / Math.PI) * 2.5
+            houses[i].position.z = Math.cos(i * 180 / Math.PI) * 2.5
+        }
+        console.log(House.houses)
+    }
+    useEffect(() => {
+        return () => {
+            House.houses = []
+        }
+    }, [])
+    return (
+        <div className="App">
+            <SceneComponent onSceneReady={onSceneReady} antialias id="mainScene"/>
+        </div>
     )
-
-    // This targets the camera to scene origin
-    camera.setPosition(new Vector3(0, 0, 20))
-
-    const canvas = scene.getEngine().getRenderingCanvas()
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true)
-
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene)
-
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.8
-    // Our built-in 'ground' shape.
-    const groundMaterial = new StandardMaterial('groundMaterial', scene)
-    groundMaterial.diffuseColor = new Color3(0, 1, 0)
-    const ground = MeshBuilder.CreateGround(
-      'ground',
-      { width: 6, height: 6 },
-      scene
-    )
-    ground.material = groundMaterial
-
-    init(scene)
-  }
-
-  const init = (scene: Scene) => {
-    const house = new House()
-    const house2 = new House()
-    house.addToScene(scene)
-    house2.addToScene(scene)
-    house.position(2, 1, 2)
-  }
-
-  return (
-    <div className="App">
-      <SceneComponent onSceneReady={onSceneReady} antialias id="mainScene" />
-    </div>
-  )
 }
 
 export default App
