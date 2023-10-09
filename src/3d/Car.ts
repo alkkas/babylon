376d92de11696e1eeff41a376475771cf38bc035
alkkas <alliscakess@gmail.com>
2023-10-09 21:12:08 +0300

    add wheels

--- src/3d/Car.ts
+++ src/3d/Car.ts
@@ -21,5 +21,22 @@ class Car {
       shape: outline,
       depth: 0.2,
     })
+    const wheelRB = MeshBuilder.CreateCylinder('wheelRB', {
+      diameter: 0.125,
+      height: 0.05,
+    })
+    wheelRB.parent = this.entity
+    wheelRB.position.z = -0.1
+    wheelRB.position.x = -0.2
+    wheelRB.position.y = 0.035
+
+    const wheelRF = wheelRB.clone('wheelRF')
+    wheelRF.position.x = 0.1
+
+    const wheelLB = wheelRB.clone('wheelLB')
+    wheelLB.position.y = -0.2 - 0.035
+
+    const wheelLF = wheelRF.clone('wheelLF')
+    wheelLF.position.y = -0.2 - 0.035
   }
 }
